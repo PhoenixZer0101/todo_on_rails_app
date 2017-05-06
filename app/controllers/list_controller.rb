f97156603754613f170cabad0aaac83cdfa062b2
Alexander Bisono <abisono@nyclabschool.org>
2017-05-06 18:42:09 +0000

    Hi

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,6 +3,6 @@ class ListController < ApplicationController
   end
   def show
     @list_name =  ['Pylons', 'Moar Pylons', 'Life Stuff', 'Other Things', 'Games and stuff'].sample
-    @list_desc = ['Pylons', 'Moar Pylons', 'Life Stuff', 'Other Things', 'Games and stuff']
+    @list_desc = ['Pylons', 'Moar Pylons', 'Life Stuff', 'Other Things', 'Games and stuff', 'POTATOES', 'Hi!']
   end
 end
