27c9affec842904d67aa1a1890033ccbacdc3218
Alexander Bisono <abisono@nyclabschool.org>
2017-05-06 17:55:10 +0000

    Osu!

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,5 +2,6 @@ class ListController < ApplicationController
   def index
   end
   def show
+    @list_name =  ['Pylons', 'Moar Pylons', 'Life Stuff', 'Other Things', 'Games and stuff'].sample
   end
 end
